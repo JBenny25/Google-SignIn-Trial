78fba8ea25baac54d0f9daec6c880084e7704589
Jubin Benny <jbenny@vmware.com>
2017-01-23 16:09:18 +0530

    GS - initial commit

---
+++ OAuth2.0/OAuth2.0/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  OAuth2.0
+//
+//  Created by Jubin Benny on 1/23/17.
+//  Copyright © 2017 JB. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Podfile
@@ -0,0 +1,10 @@
+project ‘OAuth2.0/OAuth2.0.xcodeproj', 'SecureDebug' => :debug, 'SecureRelease' => :release, 'GCov_build' => :debug
+
+source 'https://github.com/CocoaPods/Specs.git'
+
+platform :ios, '9.0'
+use_frameworks!
+
+target 'OAuth2.0' do
+	pod 'Google/SignIn'
+end
